76efb6858a0b2d3754a062e7e7db8c664cd31329
jayadev <jaidev.akkiraju@gmail.com>
2016-04-01 17:09:10 -0700

    added typeahead plugin as a bower component

--- Interactive-Design/js/script.js
+++ Interactive-Design/js/script.js
@@ -1,13 +1,27 @@
 $("document").ready(function(){
-	$("#query").on('submit',function(e){
+
+	$("#query").on('keyup',function(e){
 		e.preventDefault();
 		var address = $("#address").val();
+
+
+		$("#list").empty();
 
 		$.ajax({
 			"url":"http://104.197.66.88:9200/addresses/_search?q=Address:"+address+"~",
 			"type":"post",
 			success:function(data){
-				console.log(data.hits.hits);
+				var arr = [];
+
+				for (var i=0;i<data.hits.hits.length;i++){
+					console.log(data.hits.hits[i]._source.Address);
+					arr.push(data.hits.hits[i]._source.Address);
+
+					console.log(arr);
+					$("#address").autocomplete({
+				      source: arr
+				    });
+				}
 			},
 			error:function(err){
 
