1b620a72072abf0d8c34b1239d619261c2115081
jayadev <jaidev.akkiraju@gmail.com>
2016-04-21 01:29:02 -0700

    changed keydown to synchronous call and added ajax done method chaining

--- Interactive-Design/js/script.js
+++ Interactive-Design/js/script.js
@@ -1,56 +1,106 @@
-$("document").ready(function(){
-	$('#address').attr('autocomplete', 'off');
-	$("#query").on('keyup',function(e){
-		e.preventDefault();
-		var address = $("#address").val();
+$(function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
 
-		$("#list").empty();
+    var result = [];
+    $("#address").keyup(function(event){
+         if (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode >= 65 && event.keyCode <= 90){
+            var address = $("#address").val().toUpperCase();
+            console.log('addres length',address.length);
+             if(address.length!==0){
+                result = [];
+                $.ajax({
+                   "url":"http://104.197.66.88:9200/addresses/_search?q=Address:"+address+"~",
+                   "type":"post"
+                }).done(function(data){
+                     for (var i=0;i<data.hits.hits.length;i++){
+                       result.push(data.hits.hits[i]._source.Address);
+                     }
+                     console.log('length',result.length);
+                     console.log(result);
+                     $("#address").autocomplete({
+                        source: result
+                      });
+                });
+              }
+          }
+    });
+  });
 
-		$.ajax({
-			"url":"http://104.197.66.88:9200/addresses/_search?q=Address:"+address+"~",
-			"type":"post",
-			success:function(data){
+// $("document").ready(function(){
+// 	$("#query").on('keyup',function(e){
+// 		e.preventDefault();
+// 		var address = $("#address").val();
 
-				var source  = $("#address-template").html();
-				var template = Handlebars.compile(source);
+// 		$("#list").empty();
 
-				for (var i=0;i<data.hits.hits.length;i++){
-					var con = {
-						"address" : data.hits.hits[i]._source.Address,
-						"id" : i,
-						"address_id" : data.hits.hits[i]._id
-					};
+// 		$.ajax({
+// 			"url":"http://104.197.66.88:9200/addresses/_search?q=Address:"+address+"~",
+// 			"type":"post",
+// 			success:function(data){
 
-					var html=template(con);
-					$('#list').append(html);
+// 				var source  = $("#address-template").html();
+// 				var template = Handlebars.compile(source);
 
-				}
-			},
-			error:function(err){
-				console.log(err);
-			}
-		});
-	});
+// 				for (var i=0;i<data.hits.hits.length;i++){
+// 					var con = {
+// 						"address" : data.hits.hits[i]._source.Address,
+// 						"id" : i,
+// 						"address_id" : data.hits.hits[i]._id
+// 					};
 
-	// select a single address from the list of addresses 
-	// and make an ajax call with the single address
+// 					var html=template(con);
+// 					$('#list').append(html);
 
-	$('body').on('click','li.list-group-item',function(){
-		var selected_address = $(this).html();
-		$("#address").val(selected_address);
-		var address = selected_address;
-		$("#list").empty();
+// 				}
+// 			},
+// 			error:function(err){
+// 				console.log(err);
+// 			}
+// 		});
+// 	});
 
-		$.ajax({
-			"url":"http://104.197.66.88:9200/addresses/_search?q=Address:"+address+"~",
-			"type":"post",
-			success:function(data){
-				// show detailed information for single address
-				// we can include google maps too
-			},
-			error:function(err){
-				console.log(err);
-			}
-		});
-	});
-});
+// 	// select a single address from the list of addresses 
+// 	// and make an ajax call with the single address
+
+// 	$('body').on('click','li.list-group-item',function(){
+// 		var selected_address = $(this).html();
+// 		$("#address").val(selected_address);
+// 		var address = selected_address;
+// 		$("#list").empty();
+
+// 		$.ajax({
+// 			"url":"http://104.197.66.88:9200/addresses/_search?q=Address:"+address+"~",
+// 			"type":"post",
+// 			success:function(data){
+// 				// show detailed information for single address
+// 				// we can include google maps too
+// 			},
+// 			error:function(err){
+// 				console.log(err);
+// 			}
+// 		});
+// 	});
+// });
