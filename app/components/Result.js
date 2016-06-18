ffaaf04a7b2534bc84ea0299f359cba4718432f9
Jayadev6191 <jaidev.akkiraju@gmail.com>
2016-06-17 23:21:21 -0700

    Added css fixes

--- app/components/Result.js
+++ app/components/Result.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 export default React.createClass({
   render() {
-
     return (
       <div className="thumbnail text-center detail">
         <div>
           <span>
-            <button type="button" id="get_notified" className="btn btn-info btn-xs" data-toggle="modal" data-target="#myModal">Get Notified</button>
+            <button type="button" id="get_notified" className="btn btn-notification btn-xs" data-toggle="modal" data-target="#myModal">Get Notified</button>
           </span>
         </div>
         <h3 className="detail-address text-primary">
--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
-import Styles from './App.css'
+require("!style!css!./App.css");
 
 ReactDOM.render(<App />, document.getElementById('root'));
