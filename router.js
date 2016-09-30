cd921775d301e35f2784dc4a3ba3e9ca27007ab1
Jayanth Kanive <mailtokanive@gmail.com>
2016-09-29 23:11:22 -0700

    Add the firebase connection code

--- router.js
+++ router.js
@@ -1,5 +1,16 @@
 module.exports = function(app){
 
+  const firebase = require('firebase');
+
+  const myFirebaseRef = firebase.initializeApp({
+    databaseURL: "https://codeforsanjose-1110.firebaseio.com",
+    serviceAccount: "data/trashpickup-service.json"
+  });
+
+  const db = firebase.database();
+
+  const ref = db.ref("items");
+
   app.get('/getEnv',function(req,res){
     if(isDeveloping){
         console.log("In dev mode");
@@ -13,7 +24,15 @@ module.exports = function(app){
   app.get('/checkFirebase',function(req,res){
     if(db){
         console.log("Connection ready...");
-        res.send("Connection ready...");
+
+        ref.once("value", function(snapshot) {
+            console.log(snapshot.val());
+            console.log('------------------');
+            res.send(snapshot.val());
+            });
+
+        // res.send("Connection ready...");
+
     }else{
         console.log("Connection failed...");
         res.send("Connection failed...");
